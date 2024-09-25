cd40208c80dc384fc176e8a96437e9e8cc8a115c
ed <edgarams09@gmail.com>
2024-09-25 16:45:32 -0300

    feat: removi o módulo de erro de mensagem

--- tests/js/errorMessage.js
+++
@@ -1,7 +0,0 @@
-module.exports.logMessage = function errorMessage(messageNotOk, messageOk, error) {
-    if(error) {
-        console.error(messageNotOk);
-    } else {
-        console.log(messageOk)
-    }
-};
