0f9165d77fc9e60c3327f5e973d604e2ece0a5cd
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2024-10-06 17:59:13 -0300

    feat: removi a linha de código que chamava o elemento que continha a classe bt-save

--- src/js/dialogEvents.js
+++ src/js/dialogEvents.js
@@ -1,7 +1,6 @@
 const dialogElement = document.querySelector("dialog");
 const btSettings = document.querySelector(".bt-settings");
 const btClose = document.querySelector("dialog .bt-close");
-const btSave = document.querySelector("dialog .bt-save");
 
 // btSettings.addEventListener("click", dialogElement.showModal());
 btSettings.addEventListener("click", () => dialogElement.showModal());
