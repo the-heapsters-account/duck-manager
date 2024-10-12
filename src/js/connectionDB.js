59029c7550ceb6e74d96bf1a5f1c6728c01dd6d5
ed <edgarams09@gmail.com>
2024-10-11 21:24:54 -0300

    feat(js): criei um arquivo JS separado para a conexão de db MySQL

---
+++ src/js/connectionDB.js
@@ -0,0 +1,37 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "lc_sistemas"
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error("erro ao conectar ao banco de dados: ", err);
+        return;
+    }
+
+    console.log("conexão bem-sucedida com o MySQL!");
+});
+
+const query = 'SELECT * FROM produto WHERE id = 100001;';
+
+connection.query(query, (error, results) => {
+    if (error) {
+        console.error("Erro na consulta: ", error);
+        return;
+    }
+
+    for(const result of results) console.log(result);
+});
+
+connection.end(err => {
+    if (err) {
+        console.error("Erro ao fechar a conexão: ", err);
+        return;
+    }
+
+    console.log("conexão com o MySQL fechada");
+});
