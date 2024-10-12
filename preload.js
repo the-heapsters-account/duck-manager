e62237755f6a8e0c0f1be04748edb348dbe5314a
ed <edgarams09@gmail.com>
2024-10-12 20:59:22 -0300

    feat: adicionei o renderizador do DB para executar as queries durante a execução do programa

--- preload.js
+++ preload.js
@@ -3,4 +3,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("api", {
     readJSON: () => ipcRenderer.invoke("read-json"),
     saveJSON: data => ipcRenderer.invoke("save-json", data),
+    executeQuery: query => ipcRenderer.invoke('execute-query', query)
 });
