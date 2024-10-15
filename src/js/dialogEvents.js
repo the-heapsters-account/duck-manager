8a2021d0032ac3d21b14ab08001f58ed520d37fa
ed <edgarams09@gmail.com>
2024-10-15 11:07:05 -0300

    feat: adicionei os elementos contidos no menu de configurações

--- src/js/dialogEvents.js
+++ src/js/dialogEvents.js
@@ -1,6 +1,12 @@
 const dialogElement = document.querySelector("dialog");
-const btSettings = document.querySelector("header .bt-settings");
-const btClose = document.querySelector("dialog .bt-close");
+const btnSettings = document.querySelector("header .btn-settings");
+const btnClose = document.querySelector("dialog .btn-close");
+const configsDB = document.querySelector("#db-configs");
+const configsDBColumns = document.querySelector("#db-configs-columns");
+const themesPresentation = document.querySelector("#themes-presentation");
+const inputQuantidadeMinima = document.querySelector("#input-quantidade-minima");
+const filePathDisplay = document.querySelector("#file-path-display");
+const inputPathCopied = document.querySelector("#input-path-copied");
 
 // btSettings.addEventListener("click", dialogElement.showModal());
 btSettings.addEventListener("click", () => dialogElement.showModal());
