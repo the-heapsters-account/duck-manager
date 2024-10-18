05c7dccac6f09bbfcae07052bfb993bcd70448f5
ed <edgarams09@gmail.com>
2024-10-18 16:29:37 -0300

    style(dir): mudei o estilo do nome das pastas para camel case

---
+++ src/java/createXML/Main.java
@@ -0,0 +1,12 @@
+package createXML;
+
+// import javax.xml.bind.*;
+// import javax.xml.parsers.*;
+// import javax.xml.transform.*;
+// import org.w3c.dom.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
