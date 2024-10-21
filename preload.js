00b1a32e8d0e41442901031236b3d8a06a75013f
ed <edgarams09@gmail.com>
2024-10-21 15:27:07 -0300

    style: troquei as aspas duplas por simples

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld("api", {
-    readJSON: () => ipcRenderer.invoke("read-json"),
-    saveJSON: data => ipcRenderer.invoke("save-json", data),
     executeQuery: query => ipcRenderer.invoke('execute-query', query)
+    readJSON: () => ipcRenderer.invoke('read-json'),
+    saveJSON: data => ipcRenderer.invoke('save-json', data),
 });
