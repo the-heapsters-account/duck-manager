249ddb8de9f13910dfaea63baa54ee215e4a6ad2
Murylo Costa Santos <166061941+CSmurylo@users.noreply.github.com>
2024-10-21 20:04:20 -0300

    feat: Criei um código que cria e salva um exemplo curto de arquivo.XML na máquina.

--- src/java/createXML/Main.java
+++ src/java/createXML/Main.java
@@ -1,12 +1,58 @@
 package createXML;
 
-// import javax.xml.bind.*;
-// import javax.xml.parsers.*;
-// import javax.xml.transform.*;
-// import org.w3c.dom.*;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
-public class Main {
+public class TesteDeCriacaoXML {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+            Document documentoXML = documentBuilder.newDocument();
+
+            Element root = documentoXML.createElement("root");
+            documentoXML.appendChild(root);
+
+            Element pessoa = documentoXML.createElement("pessoa");
+            Attr id = documentoXML.createAttribute("id");
+            id.setValue("1");
+
+            pessoa.setAttributeNode(id);
+
+            root.appendChild(pessoa);
+
+            Element nome = documentoXML.createElement("nome");
+            nome.appendChild(documentoXML.createTextNode("Murylo"));
+            pessoa.appendChild(nome);
+
+            Element idade = documentoXML.createElement("idade");
+            idade.appendChild(documentoXML.createTextNode("18"));
+            pessoa.appendChild(idade);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            DOMSource documentoFonte = new DOMSource(documentoXML);
+            StreamResult documentoFinal = new StreamResult(new File("D:\\pessoa.xml"));
+
+            transformer.transform(documentoFonte, documentoFinal);
+
+            System.out.println("XML criado com sucesso!");
+
+        } catch (ParserConfigurationException | TransformerException ex) {
+            ex.printStackTrace();
+        }
     }
 }
