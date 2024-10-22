7690fcf2af14fdc848af55571ce1e482465e746b
Murylo Costa Santos <166061941+CSmurylo@users.noreply.github.com>
2024-10-21 21:27:19 -0300

    style: Adicionei comentários no código pra auxiliar na compreenssão

--- src/java/createXML/Main.java
+++ src/java/createXML/Main.java
@@ -17,36 +17,47 @@ public class TesteDeCriacaoXML {
 
     public static void main(String[] args) {
         try {
+            //criando o documento
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 
+            //novo documento em branco
             Document documentoXML = documentBuilder.newDocument();
 
+            //cria a raiz(root) e adiciona no documento
             Element root = documentoXML.createElement("root");
             documentoXML.appendChild(root);
 
+            //cria o elemento <pessoa> e o atributo "id"
             Element pessoa = documentoXML.createElement("pessoa");
             Attr id = documentoXML.createAttribute("id");
             id.setValue("1");
-
+            
+            //adiciona o "id" em pessoa
             pessoa.setAttributeNode(id);
-
+            
+            //adiciona pessoa ao root
             root.appendChild(pessoa);
 
+            //adiciona o texto "Murylo" ao <nome>
             Element nome = documentoXML.createElement("nome");
             nome.appendChild(documentoXML.createTextNode("Murylo"));
             pessoa.appendChild(nome);
 
+            //adiciona "18" em <idade>
             Element idade = documentoXML.createElement("idade");
             idade.appendChild(documentoXML.createTextNode("18"));
             pessoa.appendChild(idade);
 
+            //transformando o documento em XML
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
+            Transformer transformer = transformerFactory.newTransformer();//transformador doc. -> XML
 
             DOMSource documentoFonte = new DOMSource(documentoXML);
+            //salva o resultado final em um arquivo
             StreamResult documentoFinal = new StreamResult(new File("D:\\pessoa.xml"));
 
+            //transforma o documento num arquivo xml
             transformer.transform(documentoFonte, documentoFinal);
 
             System.out.println("XML criado com sucesso!");
