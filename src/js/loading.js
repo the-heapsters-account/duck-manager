51e75e18918df9af0c0989817327899358595d7d
ed <edgarams09@gmail.com>
2024-10-22 20:34:05 -0300

    feat(log): fiz um output do processo

--- src/js/loading.js
+++ src/js/loading.js
@@ -7,11 +7,13 @@ btnGerarDoc.addEventListener("click", () => loadingDialogElement.showModal());
 inputGerarList.addEventListener("click", async () => {
     loadingDialogElement.showModal();
     try {
+        console.log("gerando lista de pedidos...");
         const compileResponse = await window.api.compileJavaFile('readXML', 'Main.java');
         console.log(compileResponse === "" ? "código compilado com sucesso!" : null);
         const executeResponse = await window.api.executeJavaClass('readXML', 'readXML.Main');
         console.log(executeResponse);
     } catch (error) {
+        console.log("não foi possível gerar a lista de pedidos");
         console.error(error);
     } finally {
         console.log("processo finalizado")
