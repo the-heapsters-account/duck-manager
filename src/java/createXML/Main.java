885459bfc86e6ae2059249831985cecd4c5a0574
ed <edgarams09@gmail.com>
2024-10-26 22:08:53 -0300

    feat: mudei o nome da classe

--- src/java/createXML/Main.java
+++ src/java/createXML/Main.java
@@ -11,8 +11,7 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class TesteDeCriacaoXML {
-
+public class Main {
     public static void main(String[] args) {
         try {
             //criando o documento
