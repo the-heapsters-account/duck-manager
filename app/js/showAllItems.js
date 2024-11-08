4942ca929bbb7099d6ddd3d055303885564a0c12
ed <edgarams09@gmail.com>
2024-11-08 09:23:59 -0300

    feat: configurei a aparição dos produtos

--- app/js/showAllItems.js
+++ app/js/showAllItems.js
@@ -2,14 +2,44 @@ const productButton = document.querySelector('.btn-products');
 const productSection = document.querySelector('.product-container');
 
 productButton.addEventListener("click", () => {
-    window.api.getConfigs().then(settings => {
-        const tableName = settings.db_configs.table_name;
+    productSection.innerHTML = '';
+    window.api.getTableDB().then(tableSelected => {
+        window.api.getColumnsDB().then(columnsObject => {
+            try {
+                const columns = columnsObject.columnsDB
+                const columnsOrganized = columns.join(', ');
+                const query = `SELECT ${columnsOrganized} FROM ${tableSelected}`;
 
-        try {
-            window.api.executeQuery('SELECT * FROM ' + tableName).then(response => console.log(response));
-        } catch(error) {
-            alert(error);
-            console.error(error);
-        }
+                const table = document.createElement("table");
+                const tHead = document.createElement("thead");
+                const tBody = document.createElement("tbody");
+                const tHeadRow = tHead.insertRow();
+
+                columnsObject.columnsPresentation.forEach(column => {
+                    const th = document.createElement("th");
+                    tHeadRow.appendChild(th).textContent = column;
+                });
+                table.appendChild(tHead);
+
+                window.api.executeQuery(query).then(queryResults => {
+                    queryResults.forEach(queryResult => {
+                        const tBodyRow = tBody.insertRow();
+
+                        tBodyRow.insertCell().textContent = queryResult.codigo;
+                        tBodyRow.insertCell().textContent = queryResult.referencia;
+                        tBodyRow.insertCell().textContent = queryResult.codigo_barras;
+                        tBodyRow.insertCell().textContent = queryResult.nome;
+                        tBodyRow.insertCell().textContent = `R$${queryResult.preco_venda.toString().padStart(2, "0")},00`;
+                        tBodyRow.insertCell().textContent = queryResult.estoque;
+                    });
+                });
+                table.appendChild(tBody);
+
+                productSection.appendChild(table);
+            } catch(error) {
+                alert(error);
+                console.error(error);
+            }
+        });
     });
 });
