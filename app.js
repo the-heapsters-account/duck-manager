92cd0b7c00e10752b84db1a4e8f0f0c1202e583d
ed <edgarams09@gmail.com>
2024-11-10 20:18:42 -0300

    style: removi um barra do diretório

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { exec } = require('child_process');
 const paths = {
     settingsJSON: 'settings/settings.json',
     icon: 'resources/imgs/logo.png',
-    filesJava: 'lista de pedidos/src/java/',
+    dirJava: 'lista de pedidos/src/java',
     app: 'app/app.html'
 }
 
