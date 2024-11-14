c9bf36d42ff18487142eb810ce20d20bb8cde913
ed <edgarams09@gmail.com>
2024-11-14 20:07:44 -0300

    feat: criei o algoritmo de busca com JS

--- app/js/search.js
+++ app/js/search.js
@@ -1,10 +1,59 @@
-document.addEventListener('keydown', function(event) {
-    if(event.key === 'F1') {
-        event.preventDefault();
-        document.querySelector('.btn-cart').click();
+const btnSearch = document.querySelector('.btn-search');
+const inputSearch = document.querySelector('.input-search');
+
+btnSearch.addEventListener('click', () => {
+    productSection.innerHTML = '';
+    const inputSearchValue = inputSearch.value;
+
+    if(inputSearchValue.trim() === '') {
+        alert('Por favor, preencha o campo de pesquisa corretamente');
+        return;
     }
-    if(event.key === 'F2') {
-        event.preventDefault();
-        document.querySelector('.btn-settings').click();
-    }
+
+    window.api.getTableDB().then(table => {
+        window.api.getColumnsDB().then(async columnsObject => {
+            try {
+                const columns = columnsObject.columnsDB;
+                const columnsOrganized = columns.join(", ");
+                const inputSearchValueTrimed = inputSearchValue.trimStart().trimEnd();
+                const booleanQueryExpression = `UPPER('%${inputSearchValueTrimed}%') OR LOWER('%${inputSearchValueTrimed}%') OR '%${inputSearchValueTrimed}%'`;
+                const query = `SELECT ${columnsOrganized} FROM ${table} WHERE nome LIKE ${booleanQueryExpression}`;
+                const tableElements = {
+                    table: document.createElement("table"),
+                    tHead: document.createElement("thead"),
+                    tBody: document.createElement("tbody")
+                };
+                const tHeadRow = tableElements.tHead.insertRow();
+                console.log(query);
+                btnSearch.setAttribute('disabled', '');
+                inputSearch.setAttribute('disabled', '');
+
+                columnsObject.columnsPresentation.forEach(column => {
+                    const th = document.createElement("th");
+                    tHeadRow.appendChild(th).textContent = column;
+                });
+                tableElements.table.appendChild(tableElements.tHead);
+
+                await window.api.executeQuery(query).then(rows => {
+                    rows.forEach(row => {
+                        const tBodyRow = tableElements.tBody.insertRow();
+
+                        for(const column of columns) tBodyRow.insertCell().textContent = row[column];
+                    });
+                });
+
+                tableElements.table.appendChild(tableElements.tBody);
+                productSection.appendChild(tableElements.table);
+            } catch(error) {
+                alert(error);
+                console.log(error);
+
+                btnSearch.removeAttribute("disabled", "");
+                inputSearch.removeAttribute("disabled", "");
+            } finally {
+                btnSearch.removeAttribute("disabled", "");
+                inputSearch.removeAttribute("disabled", "");
+            }
+        });
+    });
 });
