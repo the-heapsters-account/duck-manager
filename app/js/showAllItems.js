4fcc3caded5dbdf50ab91bb1a0187a302a1d4ab0
ed <edgarams09@gmail.com>
2024-11-14 20:05:56 -0300

    refactor: refatorei o código usando um laço for

--- app/js/showAllItems.js
+++ app/js/showAllItems.js
@@ -21,13 +21,9 @@ productButton.addEventListener("click", () => {
 
                 await window.api.executeQuery(query).then(rows => {
                     rows.forEach(row => {
-                        const tBodyRow = tBody.insertRow();
-                        tBodyRow.insertCell().textContent = row.codigo;
-                        tBodyRow.insertCell().textContent = row.referencia;
-                        tBodyRow.insertCell().textContent = row.codigo_barras;
-                        tBodyRow.insertCell().textContent = row.nome;
-                        tBodyRow.insertCell().textContent = `R$${row.preco_venda.toFixed(2).replace('.', ',')}`;
-                        tBodyRow.insertCell().textContent = row.estoque;
+                        const tBodyRow = tableElements.tBody.insertRow();
+
+                        for(const column of columns) tBodyRow.insertCell().textContent = row[column];
                     });
                 });
 
