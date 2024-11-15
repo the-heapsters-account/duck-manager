b9d44aa46bd319f89f6d80639665620a8f52cda8
ed <edgarams09@gmail.com>
2024-11-15 14:08:23 -0300

    feat: usando a classe Tag e pegando argumentos

--- lista_de_pedidos/src/java/test/Main.java
+++ lista_de_pedidos/src/java/test/Main.java
@@ -1,12 +1,19 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length == 0) {
-            System.out.println("Nenhum argumento foi passado.");
-            return;
-        }
+        Tag tagInfos = new Tag(args[0], Arrays.asList(args[1]), Arrays.asList(args[2]), Arrays.asList(args[3]));
+
+        String fileName = tagInfos.getFileName();
+        List<String> attributesList = tagInfos.getAttributes();
+        List<String> valuesList = tagInfos.getValues();
+
+        // [0] -> nome arquivo
+        // [1] -> valor das colunas dentro da tag (<description>)
+        // [2] -> valor dos atributos das colunas de  apresentação na tag
+        // [3] -> valor dos atributos das colunas do banco de dados na tag
 
         List<DataEntry> entries = new ArrayList<>();
 
