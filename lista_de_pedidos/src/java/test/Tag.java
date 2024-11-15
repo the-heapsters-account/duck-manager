82e8d654f3a3624726aa4cc2bb63468459a99622
ed <edgarams09@gmail.com>
2024-11-15 14:18:51 -0300

    feat: adicionei novos atributos

--- lista_de_pedidos/src/java/test/Tag.java
+++ lista_de_pedidos/src/java/test/Tag.java
@@ -2,28 +2,34 @@ import java.util.List;
 
 public class Tag {
     private String fileName;
-    private List<String> attributes;
-    private List<String> values;
+    private List<String> attributesValuesTag;
+    private List<String> valuesAttributesTag;
+    private List<String> valuesTag;
 
     public Tag(String fileName, List<String> attributesValuesTag, List<String> valuesAttributesTag, List<String> valuesTag) {
         this.fileName = fileName;
-        this.attributes = attributes;
-        this.values = values;
+        this.attributesValuesTag = attributesValuesTag;
+        this.valuesAttributesTag = valuesAttributesTag;
+        this.valuesTag = valuesTag;
     }
 
     public String getFileName() {
         return this.fileName;
     }
-    public List<String> getAttributes() {
-        return this.attributes;
+    public List<String> getAttributesValuesTag() {
+        return this.attributesValuesTag;
     }
-    public List<String> getValues() {
-        return this.values;
+    public List<String> getValuesAttributesTag() {
+        return this.valuesAttributesTag;
+    }
+    public List<String> getValuesTag() {
+        return this.valuesTag;
     }
 
     public void printDates() {
         System.out.println("file name: " + this.fileName);
-        System.out.println("attributes: " + this.attributes);
-        System.out.println("values: " + this.values);
+        System.out.println("attributes values: " + this.attributesValuesTag);
+        System.out.println("values attributes: " + this.valuesAttributesTag);
+        System.out.println("values tag: " + this.valuesTag);
     }
 }
