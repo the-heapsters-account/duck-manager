27a1079f9e8635c59f5d12abbdfdd12aacd1b961
ed <edgarams09@gmail.com>
2024-11-18 17:35:48 -0300

    feat: criei uma classe cria um array list e retorna um array list com base no parãmetro passado

---
+++ lista_de_pedidos/src/java/createXML/MakeArrayList.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MakeArrayList {
+    public List<String> makeList(String input) {
+        List<String> list = new ArrayList<>(Arrays.asList(input.split(",")));
+
+        return list;
+    }
+}
