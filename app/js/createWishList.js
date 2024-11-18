27fea15a55a6c27894dc8aa5c02b4fb4aa1e488c
ed <edgarams09@gmail.com>
2024-11-18 17:26:47 -0300

    feat: removi as linhas de processos de criação da lista de pedidos

--- app/js/createWishList.js
+++ app/js/createWishList.js
@@ -20,66 +20,6 @@ inputGerarList.addEventListener("click", async () => {
         const quantidadeMinima = await window.api.getQuantidadeMinima();
         const compilesResponse = await window.api.compileJavaFile(prepareEntriesInfos.dir, prepareEntriesInfos.files);
         const compilesResponseVerify = compilesResponse === '';
-        btnGerarDoc.setAttribute('disabled', '');
-        inputGerarList.setAttribute('disabled', '');
-
-        console.log("gerando lista de pedidos...");
-
-        if(compilesResponseVerify) console.log('código compilado...');
-
-        if(compilesResponseVerify) {
-            const argsObj = {
-                fileName: null,
-                attributesNames: {
-                    db: null,
-                    spreadsheet: null
-                },
-                attributesValues: {
-                    db: null,
-                    spreadsheet: null
-                }
-            }
-            const table = await window.api.getTableDB();
-            const columnsObject = await window.api.getColumnsDB();
-            const query = `SELECT ${columnsObject.columnsDB} FROM ${table}`;
-            const rows = await window.api.executeQuery(query);
-            const getInfos = await window.api.getInfosListaPedidos();
-
-            // nome do arquivo
-            const fileName = argsObj.fileName = '"file-destiny"';
-
-            // nomes dos atributos
-            const attributesNames = {
-                db: getInfos.dbColumns.dbColumnsNames,
-                spreadsheet: getInfos.spreadsheetInfos.namesInfo
-            };
-            const attributesNamesDB = argsObj.attributesNames.db = `"${attributesNames.db.join(",")}"`;
-            const attributesNamesSpreadsheet = argsObj.attributesNames.spreadsheet = `"${attributesNames.spreadsheet.join(",")}"`;
-
-            // valores dos atributos
-            const attributesValues = {
-                db: getInfos.dbColumns.dbColumnsNamesPresentation,
-                spreadsheet: getInfos.spreadsheetInfos.namesInfoPresentation
-            };
-            const attributesValuesDB = argsObj.attributesValues.db = attributesValues.db;
-            const attributesValuesSpreadsheet = argsObj.attributesValues.spreadsheet = attributesValues.spreadsheet;
-
-            for(const row of rows) {
-                if(row.estoque <= quantidadeMinima) {
-                    const arrayToAdd = [];
-
-                    try {
-                        for(const column of columnsObject.columnsDB) arrayToAdd.push(row[column]);
-
-                        const argsItemsDB = `${attributesNamesDB} ${attributesValuesDB} "${arrayToAdd.join(',')}"`;
-                        const argsSInfoSpreadsheet = `${attributesNamesSpreadsheet.trimStart().trimEnd().replaceAll(' ', '-')} ${attributesValuesSpreadsheet}`;
-                        const args = `${fileName} ${argsItemsDB} ${argsSInfoSpreadsheet}`;
-                        const executeResponse = await window.api.executeJavaClass(prepareEntriesInfos.dir, `${prepareEntriesInfos.class} ${args}`);
-                        console.log(executeResponse);
-                    } catch (error) {
-                        alert('Erro durante compilação/execução: ' + error.message);
-                        console.error('Erro durante compilação/execução: ', error);
-                    }
                 }
             }
         } else {
